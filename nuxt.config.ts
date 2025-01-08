8a5843c1a97bdd5a10f4dccc5556debd0e2e4970
Raito5963 <181489458+Raito5963@users.noreply.github.com>
2025-01-08 08:35:05 +0000

    Add @chakra-ui/vue dependency and update nuxt.config.ts with module configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts or nuxt.config.js
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-  
+  devtools: { enabled: true },
+  modules: [
+    '@chakra-ui/nuxt',
+    '@nuxtjs/emotion'
+  ],
 })
