1b253328517668006d9b0521e60bf66253a48c12
Raito5963 <aokiju5963@gmail.com>
2025-01-08 17:39:26 +0900

    next

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// nuxt.config.ts or nuxt.config.js
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@chakra-ui/nuxt',
-    '@nuxtjs/emotion'
-  ],
-})
