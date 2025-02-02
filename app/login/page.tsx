33f9b81d1d620863c87547a60071eab9fd93a258
Raito5963 <181489458+Raito5963@users.noreply.github.com>
2025-02-02 01:55:51 +0000

    ログインページのパスワードフィールドにタイプ属性を追加

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -48,6 +48,7 @@ const Login = () => {
                         sx={{ marginBottom: 2 }}
                     />
                     <TextField
+                        type="password"
                         label="Password"
                         variant="outlined"
                         fullWidth
